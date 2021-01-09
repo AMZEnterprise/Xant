03c88dee94f36fa8188c3df6bbc14e0eb199354d
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2021-01-09 10:12:47 +0330

    Add User.cs to Xant.Core/Domain

---
+++ Xant.Core/Domain/User.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+
+namespace Xant.Core.Domain
+{
+    /// <summary>
+    /// Represents a user 
+    /// </summary>
+    public class User : IdentityUser
+    {
+        /// <summary>
+        /// Gets or sets user first name
+        /// </summary>
+        public string FirstName { get; set; }
+        /// <summary>
+        /// Gets or sets user last name
+        /// </summary>
+        public string LastName { get; set; }
+        /// <summary>
+        /// Gets or sets user biography
+        /// </summary>
+        public string Biography { get; set; }
+        /// <summary>
+        /// Gets or sets user IsActive
+        /// </summary>
+        public bool IsActive { get; set; }
+        /// <summary>
+        /// Gets or sets user FilesPathGuid
+        /// </summary>
+        public Guid FilesPathGuid { get; set; }
+        /// <summary>
+        /// Gets or sets user creation date
+        /// </summary>
+        public DateTime CreateDate { get; set; }
+        /// <summary>
+        /// Gets or sets user last edit date
+        /// </summary>
+        public DateTime LastEditDate { get; set; }
+        /// <summary>
+        /// Gets or sets user posts
+        /// </summary>
+        public ICollection<Post> Posts { get; set; }
+        /// <summary>
+        /// Gets or sets user post comments
+        /// </summary>
+        public ICollection<PostComment> PostComments { get; set; }
+    }
+}
