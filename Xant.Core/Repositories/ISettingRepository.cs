7a8b0e13e72efaf0adf958b0972f13b5b14bc44d
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2021-01-09 10:14:18 +0330

    Add ISettingRepository.cs to Xant.Core/Repositories

---
+++ Xant.Core/Repositories/ISettingRepository.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Xant.Core.Domain;
+
+namespace Xant.Core.Repositories
+{
+    /// <summary>
+    /// Represents setting repository interface 
+    /// </summary>
+    public interface ISettingRepository
+    {
+        /// <summary>
+        /// Get setting
+        /// </summary>
+        /// <returns>returns setting</returns>
+        Task<Setting> Get();
+        /// <summary>
+        /// Update setting
+        /// </summary>
+        /// <param name="setting">updated setting</param>
+        void Update(Setting setting);
+    }
+}
