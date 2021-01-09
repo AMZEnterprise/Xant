5a3d74710602e5b60a30d6cfb03b5cc1ff0dcf1c
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2021-01-09 18:31:58 +0330

    Add SettingsController.cs and Index.cshtml view to Xant.MVC/Areas/Panel

---
+++ Xant.MVC/Areas/Panel/Controllers/SettingsController.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Xant.Core;
+using Xant.Core.Domain;
+using Xant.MVC.Areas.Panel.Models.ViewModels;
+using Xant.MVC.Models.Constants;
+
+namespace Xant.MVC.Areas.Panel.Controllers
+{
+    [Area(ConstantAreas.Panel)]
+    public class SettingsController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public SettingsController(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var setting = await _unitOfWork.SettingRepository.Get();
+            return View(_mapper.Map<Setting, SettingFormViewModel>(setting));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Index(int id , SettingFormViewModel settingFormViewModel)
+        {
+            if (id != settingFormViewModel.Id)
+            {
+                return BadRequest();
+            }
+
+            if (ModelState.IsValid)
+            {
+                var setting = _mapper.Map<SettingFormViewModel, Setting>(settingFormViewModel);
+                _unitOfWork.SettingRepository.Update(setting);
+                await _unitOfWork.Complete();
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(settingFormViewModel);
+        }
+    }
+}
