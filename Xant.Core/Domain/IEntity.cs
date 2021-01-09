72bffda146ad209f3023289d929693bffc458151
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2021-01-09 10:10:35 +0330

    Add IEntity.cs to Xant.Core/Domain

---
+++ Xant.Core/Domain/IEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace Xant.Core.Domain
+{
+    /// <summary>
+    /// Represents primary key for database
+    /// </summary>
+    public interface IEntity
+    {
+        /// <summary>
+        /// Gets or sets id
+        /// </summary>
+        int Id { get; set; }
+    }
+}
