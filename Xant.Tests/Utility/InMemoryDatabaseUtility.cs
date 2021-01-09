4d05fe54589d0d1a035531ac81cb2b0ec3f24409
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2021-01-09 17:28:29 +0330

    Add InMemoryDatabaseUtility.cs to Xant.Tests/Utility

---
+++ Xant.Tests/Utility/InMemoryDatabaseUtility.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using Xant.Persistence;
+
+namespace Xant.Tests.Utility
+{
+    /// <summary>
+    /// Ef core in-memory database utility for test purposes
+    /// </summary>
+    public static class InMemoryDatabaseUtility
+    {
+        //Get in-memory database default options
+        private static DbContextOptions<ApplicationDbContext> GetDatabaseOptions()
+        {
+            return new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+        }
+
+        /// <summary>
+        /// Get in-memory database context with default options
+        /// </summary>
+        /// <returns></returns>
+        public static ApplicationDbContext GetInMemoryDatabaseContext()
+        {
+            return new ApplicationDbContext(GetDatabaseOptions());
+        }
+    }
+}
