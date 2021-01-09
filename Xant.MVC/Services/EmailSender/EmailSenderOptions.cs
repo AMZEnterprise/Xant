ce44f2ca4e28fabafb2c17582a71aab53bc415e5
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2021-01-09 17:39:22 +0330

    Add EmailSender.cs , IEmailSender.cs and EmailSenderOptions.cs and Xant.MVC/Services/EmailSender

---
+++ Xant.MVC/Services/EmailSender/EmailSenderOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace Xant.MVC.Services.EmailSender
+{
+    /// <summary>
+    /// Default options used by EmailSender.
+    /// </summary>
+    public class EmailSenderOptions
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string SmtpHost { get; set; }
+        public int SmtpPort { get; set; }
+    }
+}
---
+++ Xant.MVC/Services/EmailSender/IEmailSender.cs
@@ -0,0 +1,11 @@
+﻿namespace Xant.MVC.Services.EmailSender
+{
+    /// <summary>
+    /// Use to send email for account confirmation , password reset , and other
+    /// information data.
+    /// </summary>
+    public interface IEmailSender
+    {
+        bool SendEmail(string emailAddress, string subject, string message);
+    }
+}
