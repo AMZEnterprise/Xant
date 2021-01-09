e6fb4bb64475afa57510e5a865a5ae86fce51c8d
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2021-01-09 10:11:09 +0330

    Add Contact.cs to Xant.Core/Domain

---
+++ Xant.Core/Domain/Contact.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Xant.Core.Domain
+{
+    /// <summary>
+    /// Represents a contact form entry
+    /// </summary>
+    public class Contact : IEntity
+    {
+        public int Id { get; set; }
+        /// <summary>
+        /// Gets or sets user fullname
+        /// </summary>
+        public string UserFullName { get; set; }
+        /// <summary>
+        /// Gets or sets user email address
+        /// </summary>
+        public string Email { get; set; }
+        /// <summary>
+        /// Gets or sets contact subject
+        /// </summary>
+        public string Subject { get; set; }
+        /// <summary>
+        /// Gets or sets contact body
+        /// </summary>
+        public string Body { get; set; }
+        /// <summary>
+        /// Gets or sets user ip
+        /// </summary>
+        public string Ip { get; set; }
+        /// <summary>
+        /// Gets or sets contact creation date
+        /// </summary>
+        public DateTime CreateDate { get; set; }
+    }
+}
