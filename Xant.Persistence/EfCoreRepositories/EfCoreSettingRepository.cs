33673ec11306e61e2775325aa03e7be09c60d965
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2021-01-09 10:59:24 +0330

    Add EfCoreSettingRepository.cs to Xant.Persistence/EfCoreRepositories

---
+++ Xant.Persistence/EfCoreRepositories/EfCoreSettingRepository.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using Xant.Core.Domain;
+using Xant.Core.Repositories;
+
+namespace Xant.Persistence.EfCoreRepositories
+{
+    class EfCoreSettingRepository : ISettingRepository
+    {
+        private readonly IApplicationDbContext _context;
+        public EfCoreSettingRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Setting> Get()
+        {
+            return await _context.Settings.FirstOrDefaultAsync();
+        }
+
+        public void Update(Setting setting)
+        {
+            _context.Settings.Update(setting);
+        }
+    }
+}
