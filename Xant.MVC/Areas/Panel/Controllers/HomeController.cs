b3b60fcc699d5ba69b7268249853c797c146f7e2
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2021-01-09 17:25:33 +0330

    Add HomeController.cs and Index.cshtml to Xant.MVC/Areas/Panel

---
+++ Xant.MVC/Areas/Panel/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Xant.MVC.Models.Constants;
+
+namespace Xant.MVC.Areas.Panel.Controllers
+{
+    [Area(ConstantAreas.Panel)]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
