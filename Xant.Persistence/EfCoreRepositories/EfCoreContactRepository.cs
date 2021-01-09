5a7e1ea362fc692586a111a5322ac01f76a21b9b
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2021-01-09 10:57:53 +0330

    Add EfCoreContactRepository.cs to Xant.Persistence/EfCoreRepositories

---
+++ Xant.Persistence/EfCoreRepositories/EfCoreContactRepository.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Xant.Core.Domain;
+using Xant.Core.Repositories;
+
+namespace Xant.Persistence.EfCoreRepositories
+{
+    public class EfCoreContactRepository : IContactRepository
+    {
+        private readonly IApplicationDbContext _context;
+
+        public EfCoreContactRepository(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IQueryable<Contact> GetAll()
+        {
+            return _context.Contacts.AsQueryable();
+        }
+
+        public async Task<Contact> GetById(int id)
+        {
+            return await _context.Contacts.FindAsync(id);
+        }
+
+        public void Insert(Contact contact)
+        {
+            contact.CreateDate = DateTime.Now;
+
+            _context.Contacts.Add(contact);
+        }
+
+        public void Update(Contact contact)
+        {
+            _context.Contacts.Update(contact);
+        }
+
+        public async Task Delete(int id)
+        {
+            var contact = await GetById(id);
+            if (contact != null)
+                _context.Contacts.Remove(contact);
+        }
+
+        public async Task<int> Count()
+        {
+            return await _context.Contacts.CountAsync();
+        }
+    }
+}
