2461973206512dda67f7f3e87b02e3cd3267f6b7
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2021-01-09 16:24:17 +0330

    Add FriendlyDate.cs to Xant.MVC/Utility

---
+++ Xant.MVC/Utility/FriendlyDate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Xant.MVC.Utility
+{
+    /// <summary>
+    /// Date utility used to format dates to persian. Use it for views only.
+    /// </summary>
+    public static class FriendlyDate
+    {
+        /// <summary>
+        /// Format date to "yyyy/MM/dd" in persian
+        /// </summary>
+        /// <param name="date">english date</param>
+        /// <returns>returns "yyyy/MM/dd" date in persian</returns>
+        public static string ShortDate(DateTime date)
+        {
+            return date.ToPersianDateTime().ToString("yyyy/MM/dd");
+        }
+
+        /// <summary>
+        /// Format date to "yyyy/MM/d hh:mm:ss" in persian
+        /// </summary>
+        /// <param name="date">>english date</param>
+        /// <returns>returns "yyyy/MM/d hh:mm:ss" date in persian</returns>
+        public static string LongDate(DateTime date)
+        {
+            return date.ToPersianDigitalDateTimeString();
+        }
+
+        /// <summary>
+        ///  Format date to "hh:mm" time
+        /// </summary>
+        /// <param name="date">english date</param>
+        /// <returns>returns "hh:mm" time</returns>
+        public static string Time(DateTime date)
+        {
+            return date.ToPersianDateTime().ToString("hh:mm");
+        }
+    }
+}
