2bb7921704703b34d879e2edc20a6603040d321f
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2021-01-10 19:39:01 +0330

    Add AddLastEditDateToContact database migration to Xant.Persistence/Migrations

---
+++ Xant.Persistence/Migrations/20210110154654_AddLastEditDateToContact.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Xant.Persistence.Migrations
+{
+    public partial class AddLastEditDateToContact : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastEditDate",
+                table: "Contacts",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastEditDate",
+                table: "Contacts");
+        }
+    }
+}
