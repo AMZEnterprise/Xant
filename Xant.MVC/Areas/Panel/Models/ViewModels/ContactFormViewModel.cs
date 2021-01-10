0fb796c182b916c2a71b3afe5a2f0283ca894f02
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2021-01-10 19:43:28 +0330

    Add ContactFormViewModel.cs and ContactIndexViewModel to Xant.MVC/Areas/Panel/Models/ViewModels

---
+++ Xant.MVC/Areas/Panel/Models/ViewModels/ContactFormViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Xant.Core.Domain;
+using Xant.MVC.Models.Constants;
+
+namespace Xant.MVC.Areas.Panel.Models.ViewModels
+{
+    public class ContactFormViewModel : IEntity
+    {
+        public int Id { get; set; }
+        [Display(Name = "فرستنده")]
+        [Required(ErrorMessage = ConstantValidationErrorMessages.RequiredMsg)]
+        [MaxLength(256,ErrorMessage = ConstantValidationErrorMessages.MaxLengthMsg)]
+        public string UserFullName { get; set; }
+        [Display(Name = "موضوع")]
+        [Required(ErrorMessage = ConstantValidationErrorMessages.RequiredMsg)]
+        [MaxLength(256, ErrorMessage = ConstantValidationErrorMessages.MaxLengthMsg)]
+        public string Subject { get; set; }
+        [Display(Name = "ایمیل")]
+        [Required(ErrorMessage = ConstantValidationErrorMessages.RequiredMsg)]
+        [MaxLength(256, ErrorMessage = ConstantValidationErrorMessages.MaxLengthMsg)]
+        public string Email { get; set; }
+        [Display(Name = "توضیحات")]
+        [Required(ErrorMessage = ConstantValidationErrorMessages.RequiredMsg)]
+        [MaxLength(1000, ErrorMessage = ConstantValidationErrorMessages.MaxLengthMsg)]
+        public string Body { get; set; }
+        [Display(Name = "آیپی فرستنده")]
+        [Required(ErrorMessage = ConstantValidationErrorMessages.RequiredMsg)]
+        [MaxLength(256, ErrorMessage = ConstantValidationErrorMessages.MaxLengthMsg)]
+        public string Ip { get; set; }
+        [Display(Name = "تاریخ ایجاد")]
+        public DateTime CreateDate { get; set; }
+        [Display(Name = "تاریخ آخرین ویرایش")]
+        public DateTime LastEditDate { get; set; }
+    }
+}
---
+++ Xant.MVC/Areas/Panel/Models/ViewModels/ContactIndexViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Xant.Core.Domain;
+
+namespace Xant.MVC.Areas.Panel.Models.ViewModels
+{
+    public class ContactIndexViewModel : IEntity
+    {
+        public int Id { get; set; }
+        [Display(Name = "فرستنده")]
+        public string UserFullName { get; set; }
+        [Display(Name = "موضوع")]
+        public string Subject { get; set; }
+        [Display(Name = "ایمیل")]
+        public string Email { get; set; }
+        [Display(Name = "توضیحات")]
+        public string Body { get; set; }
+        [Display(Name = "آیپی فرستنده")]
+        public string Ip { get; set; }
+        [Display(Name = "تاریخ ایجاد")]
+        public DateTime CreateDate { get; set; }
+        [Display(Name = "تاریخ آخرین ویرایش")]
+        public DateTime LastEditDate { get; set; }
+    }
+}
