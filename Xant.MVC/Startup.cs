fe0d23b4ba5ac36b882bbbb6e640908c14c28989
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2021-01-11 10:58:05 +0330

    Configure StatusCodePagesWithReExecute in Xant.MVC\Startup.cs

--- Xant.MVC/Startup.cs
+++ Xant.MVC/Startup.cs
@@ -101,6 +101,9 @@ namespace Xant.MVC
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseStatusCodePagesWithReExecute("/Error/{0}");
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
