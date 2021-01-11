bbf2ae01ec7b45b44aa3c54d580cc0b7dd8c5c13
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2021-01-11 19:11:08 +0330

    Add FakeUserManager.cs and FakeSignInManger.cs to Xant.Tests/Mocks

---
+++ Xant.Tests/Mocks/FakeSignInManger.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+using Xant.Core.Domain;
+
+namespace Xant.Tests.Mocks
+{
+    /// <summary>
+    /// Fake sign-in manager implementation
+    /// </summary>
+    public class FakeSignInManger : SignInManager<User>
+    {
+        public FakeSignInManger()
+            : base(new FakeUserManager(),
+                new Mock<IHttpContextAccessor>().Object,
+                new Mock<IUserClaimsPrincipalFactory<User>>().Object,
+                new Mock<IOptions<IdentityOptions>>().Object,
+                new Mock<ILogger<SignInManager<User>>>().Object,
+                new Mock<IAuthenticationSchemeProvider>().Object,
+                new DefaultUserConfirmation<User>())
+        { }
+    }
+}
