3ccab7bf89e7caeb94c2dbd2f9bd81cd305a5300
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2021-01-11 11:40:09 +0330

    Add AddPhoneNumberToContact database migration to Xant.Persistence/Migrations

---
+++ Xant.Persistence/Migrations/20210111073642_AddPhoneNumberToContact.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Xant.Persistence.Migrations
+{
+    public partial class AddPhoneNumberToContact : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "Contacts",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "Contacts");
+        }
+    }
+}
