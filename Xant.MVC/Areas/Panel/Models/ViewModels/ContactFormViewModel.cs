61e037a28d625419451a59c6d9275d93d1358e63
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2021-01-11 18:41:19 +0330

    Add PhoneNumber property and remove required Email to Xant.MVC/Areas/Panel/Models/ViewModels/ContactFormViewModel.cs

--- Xant.MVC/Areas/Panel/Models/ViewModels/ContactFormViewModel.cs
+++ Xant.MVC/Areas/Panel/Models/ViewModels/ContactFormViewModel.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using Xant.Core.Domain;
 using Xant.MVC.Models.Constants;
 
 namespace Xant.MVC.Areas.Panel.Models.ViewModels
 {
-    public class ContactFormViewModel : IEntity
+    public class ContactFormViewModel : IEntity,IValidatableObject
     {
         public int Id { get; set; }
         [Display(Name = "فرستنده")]
@@ -17,20 +18,29 @@ namespace Xant.MVC.Areas.Panel.Models.ViewModels
         [MaxLength(256, ErrorMessage = ConstantValidationErrorMessages.MaxLengthMsg)]
         public string Subject { get; set; }
         [Display(Name = "ایمیل")]
-        [Required(ErrorMessage = ConstantValidationErrorMessages.RequiredMsg)]
         [MaxLength(256, ErrorMessage = ConstantValidationErrorMessages.MaxLengthMsg)]
         public string Email { get; set; }
+        [Display(Name = "تلفن")]
+        [MaxLength(20, ErrorMessage = ConstantValidationErrorMessages.MaxLengthMsg)]
+        public string PhoneNumber { get; set; }
         [Display(Name = "توضیحات")]
         [Required(ErrorMessage = ConstantValidationErrorMessages.RequiredMsg)]
         [MaxLength(1000, ErrorMessage = ConstantValidationErrorMessages.MaxLengthMsg)]
         public string Body { get; set; }
         [Display(Name = "آیپی فرستنده")]
-        [Required(ErrorMessage = ConstantValidationErrorMessages.RequiredMsg)]
-        [MaxLength(256, ErrorMessage = ConstantValidationErrorMessages.MaxLengthMsg)]
         public string Ip { get; set; }
         [Display(Name = "تاریخ ایجاد")]
         public DateTime CreateDate { get; set; }
         [Display(Name = "تاریخ آخرین ویرایش")]
         public DateTime LastEditDate { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (string.IsNullOrWhiteSpace(Email) && string.IsNullOrWhiteSpace(PhoneNumber))
+            {
+                var errorMessage = string.Format(ConstantValidationErrorMessages.RequiredMsg, "شماره تلفن یا ایمیل");
+                yield return new ValidationResult(errorMessage, new[] { nameof(PhoneNumber) });
+            }
+        }
     }
 }
