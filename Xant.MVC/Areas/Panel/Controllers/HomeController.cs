3e33df74b24487d4453da91c626484a686c90376
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2021-01-12 19:06:21 +0330

    Add [Authorize] to Xant.MVC/Areas/Panel/Controllers/HomeController.cs

--- Xant.MVC/Areas/Panel/Controllers/HomeController.cs
+++ Xant.MVC/Areas/Panel/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Xant.MVC.Models.Constants;
 
 namespace Xant.MVC.Areas.Panel.Controllers
 {
     [Area(ConstantAreas.Panel)]
+    [Authorize]
     public class HomeController : Controller
     {
         public IActionResult Index()
