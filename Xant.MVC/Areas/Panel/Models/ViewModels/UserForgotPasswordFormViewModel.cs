12c7bc5c8b0f5f81408d11cfc921485b419b3e30
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2021-01-14 11:30:52 +0330

    Add UserForgotPasswordFormViewModel.cs to Xant.MVC/Areas/Panel/Models/ViewModels

---
+++ Xant.MVC/Areas/Panel/Models/ViewModels/UserForgotPasswordFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Xant.MVC.Models.Constants;
+
+namespace Xant.MVC.Areas.Panel.Models.ViewModels
+{
+    public class UserForgotPasswordFormViewModel
+    {
+        [Display(Name = "ایمیل")]
+        [Required(ErrorMessage = ConstantValidationErrorMessages.RequiredMsg)]
+        [MaxLength(256, ErrorMessage = ConstantValidationErrorMessages.MaxLengthMsg)]
+        [EmailAddress(ErrorMessage = ConstantValidationErrorMessages.RegexMsg)]
+        public string Email { get; set; }
+    }
+}
