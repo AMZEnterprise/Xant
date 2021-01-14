e6f02e359b1866069f8e468e6e256345e82f8d59
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2021-01-14 11:56:46 +0330

    Add UrlHelperExtensions.cs to Xant.MVC/Areas/Panel/Extensions

---
+++ Xant.MVC/Areas/Panel/Extensions/UrlHelperExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Xant.MVC.Areas.Panel.Controllers;
+
+namespace Xant.MVC.Areas.Panel.Extensions
+{
+    /// <summary>
+    /// Url helper extensions
+    /// </summary>
+    public static class UrlHelperExtensions
+    {
+        /// <summary>
+        /// User password reset link generator
+        /// </summary>
+        /// <param name="urlHelper">Url helper</param>
+        /// <param name="userId">User id</param>
+        /// <param name="code">Reset code</param>
+        /// <param name="scheme">Website scheme</param>
+        /// <returns>Password reset link</returns>
+        public static string ResetPasswordCallbackLink(this IUrlHelper urlHelper, string userId, string code, string scheme)
+        {
+            return urlHelper.Action(
+                action: nameof(UsersController.ResetPassword),
+                controller: "Users",
+                values: new { userId, code },
+                protocol: scheme);
+        }
+    }
+}
