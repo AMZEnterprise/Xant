7a99fdec51df7079e269081d2e6a7b39cb724b62
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2021-01-14 11:55:27 +0330

    Add UserFormViewModelFilePathResolver.cs and UserIndexViewModelUserFullNameResolver.cs to Xant.MVC/Mappings/Resolvers

---
+++ Xant.MVC/Mappings/Resolvers/UserFormViewModelFilePathResolver.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Hosting;
+using Xant.Core.Domain;
+using Xant.MVC.Areas.Panel.Models.ViewModels;
+using Xant.MVC.Services.FileHandler;
+
+namespace Xant.MVC.Mappings.Resolvers
+{
+    /// <summary>
+    /// Auto mapper resolver for UserFormViewModel FilePath
+    /// </summary>
+    public class UserFormViewModelFilePathResolver : IValueResolver<User, UserFormViewModel, string>
+    {
+        private readonly IWebHostEnvironment _env;
+        private readonly IFileHandler _fileHandler;
+
+        public UserFormViewModelFilePathResolver(IWebHostEnvironment env, IFileHandler fileHandler)
+        {
+            _env = env;
+            _fileHandler = fileHandler;
+        }
+        public string Resolve(User source, UserFormViewModel destination, string destMember, ResolutionContext context)
+        {
+            return _fileHandler.GetFileSource(
+                _env.WebRootPath,
+                source.FilesPathGuid.ToString(),
+                FileHandlerFolder.Profiles);
+        }
+    }
+}
---
+++ Xant.MVC/Mappings/Resolvers/UserIndexViewModelUserFullNameResolver.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Xant.Core.Domain;
+using Xant.MVC.Areas.Panel.Models.ViewModels;
+
+namespace Xant.MVC.Mappings.Resolvers
+{
+    /// <summary>
+    /// Auto mapper resolver for UserIndexViewModel UserFullName
+    /// </summary>
+    public class UserIndexViewModelUserFullNameResolver : IValueResolver<User, UserIndexViewModel, string>
+    {
+        public string Resolve(User source, UserIndexViewModel destination, string destMember, ResolutionContext context)
+        {
+            return source.FirstName + " " + source.LastName;
+        }
+    }
+}
