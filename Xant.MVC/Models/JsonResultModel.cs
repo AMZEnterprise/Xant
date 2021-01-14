b021b5601355c61fae8c5a2c80ae3e690bbbba73
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2021-01-14 12:03:08 +0330

    Add JsonResultModel.cs to Xant.MVC/Models

---
+++ Xant.MVC/Models/JsonResultModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Xant.MVC.Models
+{
+    public class JsonResultModel
+    {
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+        public string RedirectUrl { get; set; }
+    }
+}
