f0c2b4f3e829968d9ff9490e50e243c32497d977
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2021-01-14 11:29:46 +0330

    Add UserIndexViewModel.cs to Xant.MVC/Areas/Panel/Models/ViewModels

---
+++ Xant.MVC/Areas/Panel/Models/ViewModels/UserIndexViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Xant.MVC.Areas.Panel.Models.ViewModels
+{
+    public class UserIndexViewModel
+    {
+        public string Id { get; set; }
+        [Display(Name = "نام کاربری")]
+        public string UserName { get; set; }
+        [Display(Name = "نام و نام خانوادگی")]
+        public string UserFullName { get; set; }
+        [Display(Name = "ایمیل")]
+        public string Email { get; set; }
+        [Display(Name = "فعال بودن")]
+        public bool IsActive { get; set; }
+        [Display(Name = "تاریخ ایجاد")]
+        public DateTime CreateDate { get; set; }
+        [Display(Name = "تاریخ آخرین ویرایش")]
+        public DateTime LastEditDate { get; set; }
+    } 
+}
