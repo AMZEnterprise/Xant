326e81aec4c2959d4cb436230f1ed1e1e232b1eb
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2021-01-14 11:30:16 +0330

    Add UserLoginFormViewModel.cs to Xant.MVC/Areas/Panel/Models/ViewModels

---
+++ Xant.MVC/Areas/Panel/Models/ViewModels/UserLoginFormViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Xant.MVC.Models.Constants;
+
+namespace Xant.MVC.Areas.Panel.Models.ViewModels
+{
+    public class UserLoginFormViewModel
+    {
+        [Display(Name = "نام کاربری")]
+        [Required(ErrorMessage = ConstantValidationErrorMessages.RequiredMsg)]
+        [StringLength(30, MinimumLength = 6, ErrorMessage = ConstantValidationErrorMessages.StringLengthMsg)]
+        public string Username { get; set; }
+        [Display(Name = "کلمه عبور")]
+        [Required(ErrorMessage = ConstantValidationErrorMessages.RequiredMsg)]
+        [StringLength(30, MinimumLength = 6, ErrorMessage = ConstantValidationErrorMessages.StringLengthMsg)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Display(Name = "مرا به خاطر بسپار")]
+        public bool RememberMe { get; set; }
+    }
+}
