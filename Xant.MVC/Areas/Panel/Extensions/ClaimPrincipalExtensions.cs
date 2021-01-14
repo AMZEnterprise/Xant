bc92d8b6430314722ad5b7799ea0521037bae481
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2021-01-14 11:34:01 +0330

    Add ClaimPrincipalExtensions.cs to Xant.MVC/Areas/Panel/Extensions

---
+++ Xant.MVC/Areas/Panel/Extensions/ClaimPrincipalExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Security.Claims;
+
+namespace Xant.MVC.Areas.Panel.Extensions
+{
+    /// <summary>
+    /// Claim principal extensions
+    /// </summary>
+    public static class ClaimPrincipalExtensions
+    {
+        /// <summary>
+        /// Get current user id
+        /// </summary>
+        /// <param name="principal">claim principal</param>
+        /// <returns></returns>
+        public static string GetUserId(this ClaimsPrincipal principal)
+        {
+            if (principal == null)
+                throw new ArgumentNullException(nameof(principal));
+
+            return principal.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        }
+    }
+}
