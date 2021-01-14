6ccae4a29ab8cc5cb4ae856d8d84d13c9422df33
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2021-01-14 11:31:47 +0330

    Add UserRoleEnumViewModel.cs to Xant.MVC/Areas/Panel/Models/ViewModels

---
+++ Xant.MVC/Areas/Panel/Models/ViewModels/UserRoleEnumViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Xant.MVC.Areas.Panel.Models.ViewModels
+{
+    public enum UserRoleEnumViewModel
+    {
+        [Display(Name = "مدیر کل", Order = 0)]
+        SuperAdmin,
+        [Display(Name = "مدیر", Order = 1)]
+        Admin,
+        [Display(Name = "نویسنده", Order = 2)]
+        Writer,
+    }
+}
