e978e1b6c7d766807a1c3ba4d343637d8c794364
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2021-01-17 13:45:44 +0330

    Add ContactViewComponent to Xant.MVC

---
+++ Xant.MVC/ViewComponents/ContactViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using AutoMapper;
+using Xant.Core;
+using Xant.Core.Domain;
+using Xant.MVC.Models.ViewModels;
+
+namespace Xant.MVC.ViewComponents
+{
+    public class ContactViewComponent : ViewComponent
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public ContactViewComponent(IUnitOfWork unitOfWork,IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var setting = await _unitOfWork.SettingRepository.Get();
+            return View(_mapper.Map<Setting, ContactViewModel>(setting));
+        }
+    }
+}
