c620f62fb9a0411895afbeadee29b7959e1d0e17
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2021-01-17 12:09:19 +0330

    Add PostCommentStatusEnumViewModel.cs to Xant.MVC/Areas/Panel/Models/ViewModels

---
+++ Xant.MVC/Areas/Panel/Models/ViewModels/PostCommentStatusEnumViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Xant.MVC.Areas.Panel.Models.ViewModels
+{
+    public enum PostCommentStatusEnumViewModel
+    {
+        [Display(Name = "نامشخص")]
+        Unclear = 1000,
+        [Display(Name = "رد شده")]
+        Rejected = 2000,
+        [Display(Name = "تایید شده")]
+        Accepted = 3000
+    }
+}
