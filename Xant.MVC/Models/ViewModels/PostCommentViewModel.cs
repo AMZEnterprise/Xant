fcdf87462697e2b0e2b64eafd8a21c03ffac4bda
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2021-01-17 13:28:07 +0330

    Add PostCommentViewModel.cs to Xant.MVC/Models/ViewModels

---
+++ Xant.MVC/Models/ViewModels/PostCommentViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xant.Core.Domain;
+
+namespace Xant.MVC.Models.ViewModels
+{
+    public class PostCommentViewModel : IEntity
+    {
+        public int Id { get; set; }
+        public int? ParentId { get; set; }
+        public string Body { get; set; }
+        public DateTime CreateDate { get; set; }
+        public bool IsEdited { get; set; }
+        public string UserFullName { get; set; }
+    }
+}
