58734832798c292273af46a36c91d1819d12effe
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2021-01-17 13:47:57 +0330

    Add PostCommentViewComponent to Xant.MVC

---
+++ Xant.MVC/ViewComponents/PostCommentViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Xant.MVC.Models.ViewModels;
+
+namespace Xant.MVC.ViewComponents
+{
+    public class PostCommentViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(PostCommentTreeViewModel postCommentTreeViewModel)
+        {
+            return await Task.FromResult(View(postCommentTreeViewModel));
+        }
+    }
+}
