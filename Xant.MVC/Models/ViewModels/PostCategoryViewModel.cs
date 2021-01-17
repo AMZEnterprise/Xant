8660efacb54d4dd70ad159d09ba14e18f91eab37
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2021-01-17 13:26:56 +0330

    Add PostCategoryViewModel.cs to Xant.MVC/Models/ViewModels

---
+++ Xant.MVC/Models/ViewModels/PostCategoryViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Xant.Core.Domain;
+
+namespace Xant.MVC.Models.ViewModels
+{
+    public class PostCategoryViewModel : IEntity
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int PostsCount { get; set; }
+    }
+}
