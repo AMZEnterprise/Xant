8d1cb293994f19dafc9f6a976db1ea091110feb5
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2021-01-17 13:26:13 +0330

    Add ContactViewModel.cs to Xant.MVC/Models/ViewModels

---
+++ Xant.MVC/Models/ViewModels/ContactViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Xant.MVC.Models.ViewModels
+{
+    public class ContactViewModel
+    {
+        public string Phone1 { get; set; }
+        public string Email1 { get; set; }
+        public ContactFormViewModel ContactFormViewModel { get; set; }
+    }
+}
