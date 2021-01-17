7fb2b46603a21da7b73f5fbe585921a241f1aef1
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2021-01-17 12:35:42 +0330

    Add Pager.cs and Pagination.cs to Xant.MVC/Models

---
+++ Xant.MVC/Models/Pagination.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Xant.MVC.Models
+{
+    public class Pagination
+    {
+        public Pager Pager { get; set; }
+
+        /// <summary>
+        /// Query string parameters dictionary
+        /// </summary>
+        public Dictionary<string, string> Params { get; set; }
+    }
+}
