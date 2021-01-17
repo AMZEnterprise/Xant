2f6200a825515266a195863fd0072b2204438347
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2021-01-17 12:40:31 +0330

    Add ErrorController.cs to Xant.MVC/Controllers

---
+++ Xant.MVC/Controllers/ErrorController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SmartBreadcrumbs.Attributes;
+
+namespace Xant.MVC.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [HttpGet("/Error/{errorCode}")]
+        [Breadcrumb("ViewData.Title")]
+        public IActionResult Index(int errorCode)
+        {
+            return View(errorCode);
+        }
+    }
+}
