3283f29c03268469d80ab2ae78cdc751ca055ff3
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2021-01-17 12:41:37 +0330

    Add PortfoliosController.cs to Xant.MVC/Controllers

---
+++ Xant.MVC/Controllers/PortfoliosController.cs
@@ -0,0 +1,73 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using SmartBreadcrumbs.Attributes;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xant.Core;
+using Xant.Core.Domain;
+using Xant.MVC.Models;
+using Xant.MVC.Models.ViewModels;
+
+namespace Xant.MVC.Controllers
+{
+    public class PortfoliosController : PostBaseController
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public PortfoliosController(
+            IUnitOfWork unitOfWork,
+            IMapper mapper) : base(unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        [Breadcrumb("نمونه کارها")]
+        public async Task<IActionResult> Index(int? page, int? postCategoryId, PostSortFilterType postSortFilterType = PostSortFilterType.SortByDateDesc)
+        {
+            var postSearch = new PostSearch()
+            {
+                PostCategoryId = postCategoryId,
+                PostTag = null,
+                PostSortFilterType = postSortFilterType
+            };
+
+            int pageSize = 6;
+
+            var posts = GetPosts(PostCategoryType.Portfolio, postSearch, pageIndex: page - 1 ?? 0, pageSize: pageSize);
+
+            var postsViewModel = new PostsViewModel()
+            {
+                PostViewModels = _mapper.Map<IEnumerable<Post>, IEnumerable<PostViewModel>>(posts),
+                Pager = new Pager(posts.TotalCount, posts.TotalPages, page, pageSize),
+                PostCategoryId = postCategoryId,
+                PostTag = null,
+                PostSortFilterType = postSortFilterType
+            };
+
+            return View(postsViewModel);
+        }
+
+        [Breadcrumb("ViewData.Title")]
+        public async Task<IActionResult> Portfolio(int id)
+        {
+            var post = await _unitOfWork.PostRepository.GetById(id);
+
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            var postViewModel = _mapper.Map<Post, PostViewModel>(post);
+
+            var postComments = await _unitOfWork.PostCommentRepository
+                .GetAllByPostId(post.Id);
+
+            postViewModel.PostCommentViewModels =
+                _mapper.Map<IEnumerable<PostComment>, IEnumerable<PostCommentViewModel>>(postComments);
+
+            return View(postViewModel);
+        }
+    }
+}
