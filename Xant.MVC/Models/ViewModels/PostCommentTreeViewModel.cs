367766935c1336279d964d91c88f5b2a5860586a
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2021-01-17 13:28:27 +0330

    Add PostCommentTreeViewModel.cs to Xant.MVC/Models/ViewModels

---
+++ Xant.MVC/Models/ViewModels/PostCommentTreeViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Xant.MVC.Models.ViewModels
+{
+    public class PostCommentTreeViewModel
+    {
+        public int? CommentSeed { get; set; }
+        public IEnumerable<PostCommentViewModel> PostCommentViewModels { get; set; }
+    }
+}
