b6b19baff7233f4e16426d1fe974e395f7a9de90
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2021-01-17 13:26:35 +0330

    Add PostCommentFormViewModel.cs to Xant.MVC/Models/ViewModels

---
+++ Xant.MVC/Models/ViewModels/PostCommentFormViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Xant.MVC.Models.Constants;
+
+namespace Xant.MVC.Models.ViewModels
+{
+    public class PostCommentFormViewModel
+    {
+        public int? ParentId { get; set; }
+        public int PostId { get; set; }
+        [Display(Name = "نام")]
+        [Required(ErrorMessage = ConstantValidationErrorMessages.RequiredMsg)]
+        [MaxLength(256, ErrorMessage = ConstantValidationErrorMessages.MaxLengthMsg)]
+        public string UserFullName { get; set; }
+        [Display(Name = "متن")]
+        [Required(ErrorMessage = ConstantValidationErrorMessages.RequiredMsg)]
+        [MaxLength(1000, ErrorMessage = ConstantValidationErrorMessages.MaxLengthMsg)]
+        public string Body { get; set; }
+        [Display(Name = "ایمیل")]
+        [Required(ErrorMessage = ConstantValidationErrorMessages.RequiredMsg)]
+        [MaxLength(256, ErrorMessage = ConstantValidationErrorMessages.MaxLengthMsg)]
+        [EmailAddress(ErrorMessage = ConstantValidationErrorMessages.RegexMsg)]
+        public string Email { get; set; }
+    }
+}
