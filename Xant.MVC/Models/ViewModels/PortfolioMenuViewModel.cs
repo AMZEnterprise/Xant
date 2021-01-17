adc9902bfa6090ed72613298925369996a73dee6
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2021-01-17 13:32:53 +0330

    Add PortfolioMenuViewModel.cs to Xant.MVC/Models/ViewModels

---
+++ Xant.MVC/Models/ViewModels/PortfolioMenuViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Xant.MVC.Models.ViewModels
+{
+    public class PortfolioMenuViewModel
+    {
+        public int? PostCategoryId { get; set; }
+        public IEnumerable<PostCategoryViewModel> PostCategoryViewModels { get; set; }
+    }
+}
