1683e558adc446b769aa78cd9944c3269fc10c1a
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2021-01-17 16:02:44 +0330

    Add ContactUsController.cs to Xant.MVC/Controllers

---
+++ Xant.MVC/Controllers/ContactUsController.cs
@@ -0,0 +1,63 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SmartBreadcrumbs.Attributes;
+using System.Threading.Tasks;
+using Xant.Core;
+using Xant.Core.Domain;
+using Xant.MVC.Models;
+using Xant.MVC.Models.Constants;
+using Xant.MVC.Models.ViewModels;
+
+namespace Xant.MVC.Controllers
+{
+    public class ContactUsController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IHttpContextAccessor _accessor;
+        private readonly IMapper _mapper;
+
+        public ContactUsController(
+            IUnitOfWork unitOfWork,
+            IHttpContextAccessor accessor,
+            IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _accessor = accessor;
+            _mapper = mapper;
+        }
+
+        [Breadcrumb("ViewData.Title")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ContactPost(ContactFormViewModel contactFormViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var contact = _mapper.Map<ContactFormViewModel, Contact>(contactFormViewModel);
+
+                contact.Ip = _accessor.HttpContext.Connection.RemoteIpAddress.ToString();
+
+                _unitOfWork.ContactRepository.Insert(contact);
+                await _unitOfWork.Complete();
+
+                return new JsonResult(new JsonResultModel()
+                {
+                    StatusCode = JsonResultStatusCode.Success,
+                    Message = ConstantMessages.ContactSentSuccessfully
+                });
+            }
+
+            return new JsonResult(new JsonResultModel()
+            {
+                StatusCode = JsonResultStatusCode.ModelSatedNotValid,
+                Message = ConstantMessages.ContactFailedToSend
+            });
+        }
+    }
+}
