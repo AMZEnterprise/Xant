482a457e3d23bfa4638bd44c7406b087e5039481
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2021-01-17 12:40:18 +0330

    Add ServicesController.cs to Xant.MVC/Controllers

---
+++ Xant.MVC/Controllers/ServicesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SmartBreadcrumbs.Attributes;
+
+namespace Xant.MVC.Controllers
+{
+    public class ServicesController : Controller
+    {
+        [Breadcrumb("ViewData.Title")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
