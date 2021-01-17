08996fdb761068f18912a87c9183fb524c492e93
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2021-01-17 13:23:03 +0330

    Add FooterViewModel.cs to Xant.MVC/Models/ViewModels

---
+++ Xant.MVC/Models/ViewModels/FooterViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Xant.MVC.Models.ViewModels
+{
+    public class FooterViewModel
+    {
+        public string Phone1 { get; set; }
+        public string Phone2 { get; set; }
+        public string Email1 { get; set; }
+        public string Email2 { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string Instagram { get; set; }
+        public string Telegram { get; set; }
+        public string GooglePlus { get; set; }
+        public string FaceBook { get; set; }
+        public string LinkedIn { get; set; }
+        public string Youtube { get; set; }
+        public string Aparat { get; set; }
+        public string GitHub { get; set; }
+    }
+}
