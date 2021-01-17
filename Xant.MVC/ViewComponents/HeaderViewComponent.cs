5a93dc0c0461da058666c4d34eea9ea98be3f291
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2021-01-17 13:46:25 +0330

    Add HeaderViewComponent to Xant.MVC

---
+++ Xant.MVC/ViewComponents/HeaderViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Xant.Core;
+using Xant.Core.Domain;
+using Xant.MVC.Models.ViewModels;
+
+namespace Xant.MVC.ViewComponents
+{
+    public class HeaderViewComponent : ViewComponent
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public HeaderViewComponent(IUnitOfWork unitOfWork,IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var setting = await _unitOfWork.SettingRepository.Get();
+            return View(_mapper.Map<Setting, HeaderViewModel>(setting));
+        }
+    }
+}
