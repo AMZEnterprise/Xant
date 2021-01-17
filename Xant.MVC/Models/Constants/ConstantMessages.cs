87a2a369fa5127cabe1ecc31c19301dc3af5e69f
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2021-01-17 12:37:31 +0330

    Refactor code in Xant.MVC/Models/Constants/ConstantMessages.cs

--- Xant.MVC/Models/Constants/ConstantMessages.cs
+++ Xant.MVC/Models/Constants/ConstantMessages.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace Xant.MVC.Models.Constants
+﻿namespace Xant.MVC.Models.Constants
 {
     public class ConstantMessages
     {
@@ -11,7 +9,6 @@ namespace Xant.MVC.Models.Constants
         public const string IdentityEmail = "ایمیل وارد شده از قبل در سیستم موجود می باشد.";
         public const string IdentityPassword = "کلمه عبور وارد شده معتبر نمی باشد. مقدار دیگری را امتحان کنید.";
         public const string IdentityAddToRoleResult = "خطایی در ثبت کاربر به وجود آمده است. لطفا بعدا تلاش کنید.";
-        public const string IdentityLoginWrongPassword = "کلمه عبور اشتباه است.";
         public const string VerificationEmailSubject = "بازیابی کلمه عبور | آژانس دیجیتال مارکتینگ ژانت";
         public const string VerificationEmailText = "برای بازیابی کلمه عبور خود بر روی لینک " + " {0}" + " کلیک کنید.";
         public const string IdentityNotRegisteredYet = "شما هنوز عضو وبسایت نشده اید.";
@@ -19,5 +16,8 @@ namespace Xant.MVC.Models.Constants
         public const string IdentityPasswordVerifyCode = "کد وارد شده با کد ارسال شده به ایمیل شما مطابقت ندارد.";
         public const string IdentityUpdatePassword = "خطایی در بروزرسانی کلمه عبور شما پیش آمده است. لطفا بعدا امتحان کنید.";
         public const string DuplicateError = "مقدار " + "{0} " + "از قبل موجود است. مقدار دیگری را امتحان کنید.";
+        public const string CommentSentAndWillShowAfterAcceptance = "نظر شما ثبت گردید و پس از تایید نمایش داده خواهد شد.";
+        public const string CommentFailedToSend = "مشکلی در ثبت نظر شما پیش آمده است. لطفا بعدا تلاش کنید.";
+
     }
 }
