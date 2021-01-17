380a55613fb431db315562ae5a07c253a38a70f9
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2021-01-17 12:28:17 +0330

    Add IPagedList and PagedList Xant.MVC/Infrastructure

---
+++ Xant.MVC/Infrastructure/IPagedList.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Xant.MVC.Infrastructure
+{
+    /// <summary>
+    /// Provides pagination functionality for views
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IPagedList<T> : IList<T>
+    {
+        int PageIndex { get; }
+        int PageSize { get; }
+        int TotalCount { get; }
+        int TotalPages { get; }
+        bool HasPreviousPage { get; }
+        bool HasNextPage { get; }
+    }
+}
