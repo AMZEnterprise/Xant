422d48a31fedd104077595c81616c6c8d0467167
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2021-01-17 12:34:11 +0330

    Add PostSearch.cs and PostSortFilterType.cs to Xant.MVC/Models

---
+++ Xant.MVC/Models/PostSearch.cs
@@ -0,0 +1,10 @@
+﻿namespace Xant.MVC.Models
+{
+    public class PostSearch
+    {
+        public string SearchString { get; set; }
+        public int? PostCategoryId { get; set; }
+        public string PostTag { get; set; }
+        public PostSortFilterType PostSortFilterType { get; set; }
+    }
+}
---
+++ Xant.MVC/Models/PostSortFilterType.cs
@@ -0,0 +1,8 @@
+﻿namespace Xant.MVC.Models
+{
+    public enum PostSortFilterType
+    {
+        SortByDateAsc = 1000,
+        SortByDateDesc = 2000
+    }
+}
