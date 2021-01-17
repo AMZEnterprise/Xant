be355316f74e1921df6ff9dc30451256021e2908
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2021-01-17 13:33:15 +0330

    Add BlogSidebarViewModel.cs to Xant.MVC/Models/ViewModels

---
+++ Xant.MVC/Models/ViewModels/BlogSidebarViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Xant.MVC.Models.ViewModels
+{
+    public class BlogSidebarViewModel
+    {
+        public IEnumerable<PostViewModel> PostViewModels { get; set; }
+        public IEnumerable<PostCategoryViewModel> PostCategoryViewModels { get; set; }
+        public IEnumerable<string> PostTags { get; set; }
+        [Display(Name = "جستجوی مطلب ...")]
+        public string SearchString { get; set; }
+    }
+}
