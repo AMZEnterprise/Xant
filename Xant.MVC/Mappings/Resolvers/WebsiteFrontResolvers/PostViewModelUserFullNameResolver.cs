43da19124fa55d981fedd6abe8b82be55d8c8d6c
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2021-01-17 12:32:18 +0330

    Add PostViewModelUserFullNameResolver.cs to Xant.MVC/Mappings/Resolvers/WebsiteFrontResolvers

---
+++ Xant.MVC/Mappings/Resolvers/WebsiteFrontResolvers/PostViewModelUserFullNameResolver.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Xant.Core.Domain;
+using Xant.MVC.Models.ViewModels;
+
+namespace Xant.MVC.Mappings.Resolvers.WebsiteFrontResolvers
+{
+    /// <summary>
+    /// Auto mapper resolver for PostViewModel UserFullName
+    /// </summary>
+    public class PostViewModelUserFullNameResolver : IValueResolver<Post, PostViewModel, string>
+    {
+        public string Resolve(Post source, PostViewModel destination, string destMember, ResolutionContext context)
+        {
+            return source.User.FirstName + " " + source.User.LastName;
+        }
+    }
+}
