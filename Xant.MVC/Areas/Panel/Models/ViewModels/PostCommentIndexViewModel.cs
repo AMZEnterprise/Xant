383185fe50762377e01ff3110025d219de02e1b7
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2021-01-17 12:08:45 +0330

    Add PostCommentIndexViewModel.cs to Xant.MVC/Areas/Panel/Models/ViewModels

---
+++ Xant.MVC/Areas/Panel/Models/ViewModels/PostCommentIndexViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Xant.Core.Domain;
+
+namespace Xant.MVC.Areas.Panel.Models.ViewModels
+{
+    public class PostCommentIndexViewModel : IEntity
+    {
+        public int Id { get; set; }
+        [Display(Name = "فرستنده")]
+        public string UserFullName { get; set; }
+        [Display(Name = "عنوان مطلب")]
+        public string PostTitle { get; set; }
+        [Display(Name = "متن")]
+        public string Body { get; set; }
+        [Display(Name = "ایمیل")]
+        public string Email { get; set; }
+        [Display(Name = "وضعیت")]
+        public PostCommentStatusEnumViewModel Status { get; set; }
+        [Display(Name = "آیپی فرستنده")]
+        public string Ip { get; set; }
+        [Display(Name = "ویرایش شده")]
+        public bool IsEdited { get; set; }
+        [Display(Name = "تاریخ ایجاد")]
+        public DateTime CreateDate { get; set; }
+        [Display(Name = "تاریخ آخرین ویرایش")]
+        public DateTime LastEditDate { get; set; }
+    }
+}
