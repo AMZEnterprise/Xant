1d3e50c6517ba3f61b80a9215779545452191add
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2021-01-17 12:32:46 +0330

    Add PostCommentIndexViewModelUserFullNameResolver.cs to Xant.MVC/Mappings/Resolvers

---
+++ Xant.MVC/Mappings/Resolvers/PostCommentIndexViewModelUserFullNameResolver.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Xant.Core.Domain;
+using Xant.MVC.Areas.Panel.Models.ViewModels;
+
+namespace Xant.MVC.Mappings.Resolvers
+{
+    /// <summary>
+    /// Auto mapper resolver for PostCommentIndexViewModel UserFullName
+    /// </summary>
+    public class PostCommentIndexViewModelUserFullNameResolver : IValueResolver<PostComment, PostCommentIndexViewModel, string>
+    {
+        public string Resolve(PostComment source, PostCommentIndexViewModel destination, string destMember, ResolutionContext context)
+        {
+            return source.User == null ? source.UserFullName : source.User.FirstName + " " + source.User.LastName;
+        }
+    }
+}
