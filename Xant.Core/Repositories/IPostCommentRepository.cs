6b9f677a1504b7010e9fe0b10aad6128502daf78
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2021-01-17 11:45:52 +0330

    Add GetAllByPostId() to IPostCommentRepository.cs

--- Xant.Core/Repositories/IPostCommentRepository.cs
+++ Xant.Core/Repositories/IPostCommentRepository.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Xant.Core.Domain;
 
@@ -14,6 +15,12 @@ namespace Xant.Core.Repositories
         /// </summary>
         /// <returns>returns all post comments</returns>
         IQueryable<PostComment> GetAll();
+        /// <summary>
+        /// Get a post all comments
+        /// </summary>
+        /// <param name="postId">post id</param>
+        /// <returns>return an IEnumerable of the post comments</returns>
+        Task<IEnumerable<PostComment>> GetAllByPostId(int postId);
         /// <summary>
         /// Get a post comment by id
         /// </summary>
@@ -31,11 +38,24 @@ namespace Xant.Core.Repositories
         /// <param name="postComment">updated post comment</param>
         void Update(PostComment postComment);
         /// <summary>
+        /// Update a post comment all children status
+        /// </summary>
+        /// <param name="id">post comment id</param>
+        /// <param name="status">updated status</param>
+        /// <returns></returns>
+        Task UpdateChildrenStatus(int id, PostCommentStatus status);
+        /// <summary>
         /// Delete a post comment by id
         /// </summary>
         /// <param name="id">post comment id</param>
         /// <returns></returns>
         Task Delete(int id);
+        /// <summary>
+        /// Delete a post comment all children
+        /// </summary>
+        /// <param name="id">post comment id</param>
+        /// <returns></returns>
+        Task DeleteChildren(int id);
         /// <summary>
         /// Count total post comments
         /// </summary>
