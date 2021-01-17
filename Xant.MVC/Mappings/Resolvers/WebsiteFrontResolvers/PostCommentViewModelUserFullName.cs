bcb63e223cf9547870f1290f56292ee527c7d3c9
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2021-01-17 12:30:59 +0330

    Add PostCommentViewModelUserFullName.cs to Xant.MVC/Mappings/Resolvers/WebsiteFrontResolvers

---
+++ Xant.MVC/Mappings/Resolvers/WebsiteFrontResolvers/PostCommentViewModelUserFullName.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Xant.Core.Domain;
+using Xant.MVC.Models.ViewModels;
+
+namespace Xant.MVC.Mappings.Resolvers.WebsiteFrontResolvers
+{
+    /// <summary>
+    /// Auto mapper resolver for PostCommentViewModel UserFullName
+    /// </summary>
+    public class PostCommentViewModelUserFullName : IValueResolver<PostComment, PostCommentViewModel, string>
+    {
+        public string Resolve(PostComment source, PostCommentViewModel destination, string destMember, ResolutionContext context)
+        {
+            return source.User == null ? source.UserFullName : source.User.FirstName + " " + source.User.LastName;
+        }
+    }
+}
