a12221ca3a7a0bc821ea01524860e4c3d6df124a
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2021-01-17 13:48:38 +0330

    Add ServicesViewComponent to Xant.MVC

---
+++ Xant.MVC/ViewComponents/ServicesViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Xant.MVC.ViewComponents
+{
+    public class ServicesViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return await Task.FromResult(View());
+        }
+    }
+}
