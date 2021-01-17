d5e6c4cb2c935d7d2763c31293b88d05e81b5bc8
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2021-01-17 13:24:29 +0330

    Add HeaderViewModel.cs to Xant.MVC/Models/ViewModels

---
+++ Xant.MVC/Models/ViewModels/HeaderViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Xant.MVC.Models.ViewModels
+{
+    public class HeaderViewModel
+    {
+        public string Phone1 { get; set; }
+        public string Instagram { get; set; }
+        public string Telegram { get; set; }
+    }
+}
