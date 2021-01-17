eb5e782d74ce644c071cb3051e078f8354d35365
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2021-01-17 12:31:59 +0330

    Add PostViewModelUserFilePathResolver.cs to Xant.MVC/Mappings/Resolvers/WebsiteFrontResolvers

---
+++ Xant.MVC/Mappings/Resolvers/WebsiteFrontResolvers/PostViewModelUserFilePathResolver.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Hosting;
+using Xant.Core.Domain;
+using Xant.MVC.Models.ViewModels;
+using Xant.MVC.Services.FileHandler;
+
+namespace Xant.MVC.Mappings.Resolvers.WebsiteFrontResolvers
+{
+    /// <summary>
+    /// Auto mapper resolver for PostViewModel UserFilePath
+    /// </summary>
+    public class PostViewModelUserFilePathResolver : IValueResolver<Post, PostViewModel, string>
+    {
+        private readonly IWebHostEnvironment _env;
+        private readonly IFileHandler _fileHandler;
+
+        public PostViewModelUserFilePathResolver(IWebHostEnvironment env, IFileHandler fileHandler)
+        {
+            _env = env;
+            _fileHandler = fileHandler;
+        }
+        public string Resolve(Post source, PostViewModel destination, string destMember, ResolutionContext context)
+        {
+            return _fileHandler.GetFileSource(
+                _env.WebRootPath,
+                source.User.FilesPathGuid.ToString(),
+                FileHandlerFolder.Profiles);
+        }
+    }
+}
