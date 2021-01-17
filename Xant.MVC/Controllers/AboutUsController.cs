e44a56bbeacd1d2d8d6b79480d18e120b05153cf
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2021-01-17 12:40:03 +0330

    Add AboutUsController.cs to Xant.MVC/Controllers

---
+++ Xant.MVC/Controllers/AboutUsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SmartBreadcrumbs.Attributes;
+
+namespace Xant.MVC.Controllers
+{
+    public class AboutUsController : Controller
+    {
+        [Breadcrumb("وبلاگ")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
