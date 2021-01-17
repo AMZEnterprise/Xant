bcd4583da12873c50cd3455fdda6c0c2c9fb57ba
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2021-01-17 12:09:03 +0330

    Add PostCommentReplyFormViewModel.cs to Xant.MVC/Areas/Panel/Models/ViewModels

---
+++ Xant.MVC/Areas/Panel/Models/ViewModels/PostCommentReplyFormViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Xant.MVC.Models.Constants;
+
+namespace Xant.MVC.Areas.Panel.Models.ViewModels
+{
+    public class PostCommentReplyFormViewModel
+    {
+        public int? PostId { get; set; }
+        public int ParentId { get; set; }
+        [Display(Name = "نظر پدر")]
+        public string ParentBody { get; set; }
+        [Display(Name = "متن")]
+        [Required(ErrorMessage = ConstantValidationErrorMessages.RequiredMsg)]
+        [MaxLength(1000, ErrorMessage = ConstantValidationErrorMessages.MaxLengthMsg)]
+        public string Body { get; set; }
+        [Display(Name = "وضعیت")]
+        public PostCommentStatusEnumViewModel Status { get; set; }
+    }
+}
