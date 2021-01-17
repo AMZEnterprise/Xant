03989a382b11a9b622c0e4db2d545e38a092e6f9
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2021-01-17 12:04:48 +0330

    Refactor code in Xant.MVC/wwwroot/js/active.js

--- Xant.MVC/wwwroot/js/active.js
+++ Xant.MVC/wwwroot/js/active.js
@@ -162,7 +162,7 @@
         $('.profile-circle-wrapper').addClass('circle-animation');
         $('.profile-icon').fadeIn();
     });
-    
+
     // :: 13.0 REVIEWS ACTIVE CODE
     $('.client-reviews.owl-carousel').owlCarousel({
         loop: true,
@@ -192,24 +192,24 @@
     });
 
     // :: 14.0 PORTFOLIO ACTIVE CODE
-    $('.portfolio-area').each(function(index) {
+    $('.portfolio-area').each(function (index) {
 
         var count = index + 1;
 
-        $(this).find('.portfolio-items').removeClass('portfolio-items').addClass('portfolio-items-'+count);
-        $(this).find('.portfolio-item').removeClass('portfolio-item').addClass('portfolio-item-'+count);
-        $(this).find('.portfolio-btn').removeClass('portfolio-btn').addClass('portfolio-btn-'+count);
-        
+        $(this).find('.portfolio-items').removeClass('portfolio-items').addClass('portfolio-items-' + count);
+        $(this).find('.portfolio-item').removeClass('portfolio-item').addClass('portfolio-item-' + count);
+        $(this).find('.portfolio-btn').removeClass('portfolio-btn').addClass('portfolio-btn-' + count);
+
         var Shuffle = window.Shuffle;
-        var Filter  = new Shuffle(document.querySelector('.portfolio-items-'+count), {
-            itemSelector: '.portfolio-item-'+count,
+        var Filter = new Shuffle(document.querySelector('.portfolio-items-' + count), {
+            itemSelector: '.portfolio-item-' + count,
             buffer: 1,
         })
-    
-        $('.portfolio-btn-'+count).on('change', function (e) {
-    
+
+        $('.portfolio-btn-' + count).on('change', function (e) {
+
             var input = e.currentTarget;
-            
+
             if (input.checked) {
                 Filter.filter(input.value);
             }
