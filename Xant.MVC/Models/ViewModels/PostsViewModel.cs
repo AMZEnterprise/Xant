3038b611cd5cf26604da8bea39fd763024f8d49b
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2021-01-17 13:29:15 +0330

    Add PostsViewModel.cs to Xant.MVC/Models/ViewModels

---
+++ Xant.MVC/Models/ViewModels/PostsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Xant.MVC.Models.ViewModels
+{
+    public class PostsViewModel
+    {
+        public IEnumerable<PostViewModel> PostViewModels { get; set; }
+        public Pager Pager { get; set; }
+        public string SearchString { get; set; }
+        public int? PostCategoryId { get; set; }
+        public string PostTag { get; set; }
+        public PostSortFilterType PostSortFilterType { get; set; }
+    }
+}
